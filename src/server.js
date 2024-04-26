b68bf96a59b0767cd8543dd1b9f552b8e8e84e8a
Alexander Lead <a.lead@yandex.ru>
2024-04-26 08:58:30 +0300

    feat(main): complete ssr example

--- src/server.js
+++ src/server.js
@@ -1,22 +1,30 @@
-const path = required('path');
+const path = require('path');
 const express = require('express');
 const { createSSRApp } = require('vue')
+const {renderToString } = require('@vue/server-renderer')
 const manifest = require('../dist/ssr-manifest.json');
 
 const server =  express();
 
 const appPath = path.join(__dirname, '../dist', manifest['app.js'] );
-const App = required(appPath).default;
+const App = require(appPath).default;
 
-server.get( '*', (req, res) => {
+server.use("/img", express.static(path.join(__dirname, "../dist", "img")))
+server.use("/js", express.static(path.join(__dirname, "../dist", "js")))
+server.use("/css", express.static(path.join(__dirname, "../dist", "css")))
+server.use("/favicon.ico", express.static(path.join(__dirname, "../dist", "favicon.ico")))
+
+server.get( '*', async (req, res) => {
     const app = createSSRApp(App);
+    const appContent = await renderToString(app)
     const html = `
    <html>
    <head>
    <title>VUE SSR Component</title>
+   <link rel="stylesheet" href="${manifest['app.css']}" />
    </head>
    <body>
-   This is Vue SSR Component
+    ${appContent}
    </body>
    </html>
     `;
